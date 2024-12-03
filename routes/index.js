2c7cbd4abc47430647b3befa666b8efb95350670
zohaibxJackie <zkhan52607@gmail.com>
2024-12-03 22:32:54 +0500

    package updated

--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,79 @@
-/* jshint esversion: 6 */
-/* jshint esversion: 8 */
-
 var express = require('express');
 var router = express.Router();
 var userModel = require('./users');
 var studentModel = require('./student');
-
 var passport = require('passport');
-var localStrategy = require('passport-local');
-passport.use(new localStrategy(userModel.authenticate()));
-
+var LocalStrategy = require('passport-local').Strategy;
 const upload = require('./multer');
+
+// Passport configuration
+passport.use(new LocalStrategy(userModel.authenticate()));
+
+// Middleware to check if user is logged in
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
+}
+
 /* GET home page. */
-router.get('/', function (req, res) {
-  res.render('index', { PageTitle: "Register Admin" });
+router.get('/', (req, res) => {
+  try {
+    res.render('index', { PageTitle: "Register Admin" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error occurred");
+  }
 });
 
-router.get('/login', function (req, res) {
-  res.render('login', { error: req.flash('error'), PageTitle: "Login" });
+router.get('/login', (req, res) => {
+  try {
+    res.render('login', { error: req.flash('error'), PageTitle: "Login" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error occurred");
+  }
 });
 
-router.get('/home', isLoggedIn, function (req, res) {
-  res.render('Home', { PageTitle: "Home" });
+router.get('/home', isLoggedIn, (req, res) => {
+  try {
+    res.render('home', { PageTitle: "Home" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error occurred");
+  }
 });
 
-router.get('/update', isLoggedIn, function (req, res, next) {
-  res.render('update', { PageTitle: "Upadate" });
+router.get('/update', isLoggedIn, (req, res) => {
+  try {
+    res.render('update', { PageTitle: "Update" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error occurred");
+  }
 });
 
-router.get('/delete', isLoggedIn, function (req, res, next) {
-  res.render('delete', { PageTitle: "Delete Student" });
+router.get('/delete', isLoggedIn, (req, res) => {
+  try {
+    res.render('delete', { PageTitle: "Delete Student" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error occurred");
+  }
 });
 
-router.get('/search', isLoggedIn, function (req, res, next) {
-  res.render('search', { PageTitle: "Search Student" });
+router.get('/search', isLoggedIn, (req, res) => {
+  try {
+    res.render('search', { PageTitle: "Search Student" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error occurred");
+  }
 });
 
-// register the student
-router.post('/registerStudent',upload.single('image'), async function (req, res) {
+// Register the student
+router.post('/registerStudent', upload.single('image'), async (req, res) => {
   try {
     await studentModel.create({
       name: req.body.name,
@@ -46,16 +82,16 @@ router.post('/registerStudent',upload.single('image'), async function (req, res)
     });
     res.redirect("/home");
   } catch (error) {
+    console.log(error);
     if (error.code === 11000) {
-      res.status(400).send("This Roll no. already exists");
+      res.status(400).send("This Roll number already exists");
     } else {
-      console.log(error);
-      res.status(500).send("some error occurred");
+      res.status(500).send("Some error occurred");
     }
   }
 });
 
-router.post('/search_result', async function (req, res) {
+router.post('/search_result', async (req, res) => {
   try {
     let result = await studentModel.findOne({ rollNumber: req.body.search }).populate('image');
     if (result) {
@@ -64,72 +100,76 @@ router.post('/search_result', async function (req, res) {
       res.send("No such student with this roll number");
     }
   } catch (error) {
-    res.status(500).send("Internet or server errror");
+    console.log(error);
+    res.status(500).send("Internet or server error");
   }
 });
 
-router.post('/update', async function (req, res) {
+router.post('/update', async (req, res) => {
   try {
-    await studentModel.updateOne({ rollNumber: req.body.old_num }, { $set: { rollNumber: req.body.new_num } })
-      .then(updatedUser => {
-        res.send("Student is updated successfully");
-      })
-      .catch(errror => {
-        res.send('some error occured');
-      });
+    await studentModel.updateOne({ rollNumber: req.body.old_num }, { $set: { rollNumber: req.body.new_num } });
+    res.send("Student updated successfully");
   } catch (error) {
-    router.send('Internet or server error');
+    console.log(error);
+    res.send('Internet or server error');
   }
 });
 
-router.post('/delete', async function (req, res) {
+router.post('/delete', async (req, res) => {
   try {
-    await studentModel.deleteOne({ rollNumber: req.body.roll_num })
-      .then(deleted => {
-        res.send("student deleted successfully");
+    await studentModel.deleteOne({ rollNumber: req.body.roll_num });
+    res.send("Student deleted successfully");
+  } catch (error) {
+    console.log(error);
+    res.send("Internet or server issue");
+  }
+});
+
+// This is for admin only
+router.post('/register', (req, res, next) => {
+  try {
+    if (req.body.adminKey !== "123abc") {
+      res.send("Not admin");
+      return;
+    }
+    const userData = new userModel({ username: req.body.username });
+
+    userModel.register(userData, req.body.password)
+      .then(() => {
+        passport.authenticate('local')(req, res, () => {
+          res.redirect('/home');
+        });
       })
       .catch(error => {
-        res.send("Roll number is invalid");
+        console.log(error);
+        res.send("Registration error");
       });
   } catch (error) {
-    res.send("internet or server issue");
+    console.log(error);
+    res.status(500).send("An error occurred");
   }
-});
-
-// this is for admin only
-router.post('/register', function (req, res, next) {
-  if (req.body.adminKey !== "123abc") {
-    res.send("not admin");
-    return;
-  }
-  const { username } = req.body;
-  const userData = new userModel({ username });
-
-  userModel.register(userData, req.body.password)
-    .then(function () {
-      passport.authenticate('local')(req, res, function () {
-        res.redirect('/home');
-      });
-    });
 });
 
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/home',
   failureRedirect: '/login',
   failureFlash: true
-}), function (res, req) { });
+}));
 
-router.get('/logout', function (req, res) {
-  req.logout(function (err) {
-    if (err) { return next(err); }
-    res.redirect('/');
-  });
+// Logout route
+router.get('/logout', (req, res) => {
+  try {
+    req.logout((err) => {
+      if (err) {
+        console.log(err);
+        return next(err);
+      }
+      res.redirect('/');
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error occurred");
+  }
 });
-
-
-function isLoggedIn(req, res, next) {
-  if (req.isAuthenticated()) return next();
-  res.redirect('/');
-}
 
 module.exports = router;
